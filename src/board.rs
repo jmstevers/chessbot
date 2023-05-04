112c63475857dee24f209ad1ee9405054f2505a7
Johnathan Stevers <jmstevers@gmail.com>
2023-05-04 15:06:57 +0000

    piece centric -> board centric

--- src/board.rs
+++ src/board.rs
@@ -1,5 +1,90 @@
-use crate::piece::Piece;
+use crate::piece::{Piece, PieceType, Color};
 
 pub struct Board {
     tiles: [[Piece; 8]; 8],
+}
+
+impl Board {
+    // pub fn new() -> Board {
+    //     let tiles = [[Piece::new(PieceType::Rook, Color::Black)],[],[],[],[],[],[],[]]
+    // }
+
+    pub fn move_piece(&mut self, from: (usize, usize), to: (usize, usize), take: bool) -> Result<(), String> {
+        if to.0 > 8 || to.1 > 8  {
+            return Err("Invalid board square (out of bounds)".into());
+        }
+
+        match self.tiles[from.0][from.1].piece_type {
+            PieceType::Empty => Err("Can not move empty square".into()),
+            PieceType::Pawn => self.move_pawn(from, to, take),
+            PieceType::Knight => self.move_knight(from, to, take),
+            PieceType::Bishop => self.move_bishop(from, to, take),
+            PieceType::Rook => self.move_rook(from, to, take),
+            PieceType::Queen => self.move_queen(from, to, take),
+            PieceType::King => self.move_king(from, to, take),
+        }
+    }
+
+    fn move_pawn(&mut self, from: (usize, usize), to: (usize, usize), take: bool) -> Result<(), String> {
+        let delta = (to.0 - from.0, to.1 - from.1);
+
+        println!("{delta:?}");
+
+        match (self.tiles[from.0][from.1].color, take) {
+            (Color::White, false) => {
+                if to.1 < from.1 {
+                    return Err("Can't move pawn backwards.".into())
+                }
+                todo!()
+            },
+            (Color::Black, false) => {
+                if to.1 > from.1 {
+                    return Err("Can't move pawn backwards.".into())
+                }
+                todo!()
+            }
+            (Color::White, true) => {
+                if to.1 < from.1 {
+                    return Err("Pawn can't take backwards.".into())
+                }
+                todo!()
+            },
+            (Color::Black, true) => {
+                if to.1 > from.1 {
+                    return Err("Pawn can't take backwards.".into())
+                }
+                todo!()
+            }
+        }
+    }
+
+    fn move_knight(&mut self, from: (usize, usize), to: (usize, usize), take: bool) -> Result<(), String> {
+        todo!()
+    }
+
+    fn move_bishop(&mut self, from: (usize, usize), to: (usize, usize), take: bool) -> Result<(), String> {
+        todo!()
+    }
+
+    fn move_rook(&mut self, from: (usize, usize), to: (usize, usize), take: bool) -> Result<(), String> {
+        todo!()
+    }
+
+    fn move_queen(&mut self, from: (usize, usize), to: (usize, usize), take: bool) -> Result<(), String> {
+        todo!()
+    }
+
+    fn move_king(&mut self, from: (usize, usize), to: (usize, usize), take: bool) -> Result<(), String> {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn pawn() {
+        move_pawn()
+    }
 }
--- src/piece.rs
+++ src/piece.rs
@@ -1,6 +1,12 @@
 use crate::board::Board;
 
-enum PieceType {
+#[derive(Copy, Clone)]
+pub enum Color {
+    White,
+    Black,
+}
+
+pub enum PieceType {
     Empty,
     Pawn,
     Knight,
@@ -11,49 +17,12 @@ enum PieceType {
 }
 
 pub struct Piece {
-    type_of: PieceType,
-    position: (usize, usize),
-    color: bool,
+    pub piece_type: PieceType,
+    pub color: Color,
 }
 
 impl Piece {
-    pub fn move_to(&mut self, board: Board, to: (usize, usize), take: bool) -> Result<(), String> {
-        if to.0 > 8 || to.1 > 8 {
-            return Err("Invalid board square (out of bounds)".into());
-        }
-
-        match self.type_of {
-            PieceType::Empty => Err("Can not move empty square".into()),
-            PieceType::Pawn => self.move_pawn(board, to, take),
-            PieceType::Knight => self.move_knight(board, to, take),
-            PieceType::Bishop => self.move_bishop(board, to, take),
-            PieceType::Rook => self.move_rook(board, to, take),
-            PieceType::Queen => self.move_queen(board, to, take),
-            PieceType::King => self.move_king(board, to, take),
-        }
-    }
-
-    fn move_pawn(&mut self, board: Board, to: (usize, usize), take: bool) -> Result<(), String> {
-        todo!()
-    }
-
-    fn move_knight(&mut self, board: Board, to: (usize, usize), take: bool) -> Result<(), String> {
-        todo!()
-    }
-
-    fn move_bishop(&mut self, board: Board, to: (usize, usize), take: bool) -> Result<(), String> {
-        todo!()
-    }
-
-    fn move_rook(&mut self, board: Board, to: (usize, usize), take: bool) -> Result<(), String> {
-        todo!()
-    }
-
-    fn move_queen(&mut self, board: Board, to: (usize, usize), take: bool) -> Result<(), String> {
-        todo!()
-    }
-
-    fn move_king(&mut self, board: Board, to: (usize, usize), take: bool) -> Result<(), String> {
-        todo!()
+    pub fn new(piece_type: PieceType, color: Color) -> Piece {
+        Piece { piece_type, color }
     }
 }
